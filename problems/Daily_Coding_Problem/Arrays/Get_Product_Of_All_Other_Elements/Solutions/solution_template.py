8818f43e1adeb93580239298d2d88571046d804f
jonathan.westcott <jonathan.westcott123@hotmail.co.uk>
2021-04-06 09:27:14 +0100

    Removed Camel Case...

--- problems/Daily_Coding_Problem/Arrays/Get_Product_Of_All_Other_Elements/Solutions/solution_template.py
+++ problems/Daily_Coding_Problem/Arrays/Get_Product_Of_All_Other_Elements/Solutions/solution_template.py
@@ -1,4 +1,4 @@
-def assertEquals(expected: object, received: object) -> bool:
+def assert_equals(expected: object, received: object) -> bool:
     print("Expected: {}, Received: {}".format(expected, received))
     return expected == received
 
@@ -8,5 +8,5 @@ def product_of_all_other_elements(arr: list) -> list:
 
 
 if __name__ == "__main__":
-    assertEquals([120, 60, 40, 30, 24], product_of_all_other_elements([1, 2, 3, 4, 5]))
-    assertEquals([3, 2, 1], product_of_all_other_elements([2, 3, 6]))
+    assert_equals([120, 60, 40, 30, 24], product_of_all_other_elements([1, 2, 3, 4, 5]))
+    assert_equals([3, 2, 1], product_of_all_other_elements([2, 3, 6]))
