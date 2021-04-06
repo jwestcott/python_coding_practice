4e2b47a31b46d255a69b28b20370913bc2b084b4
jonathan.westcott <jonathan.westcott123@hotmail.co.uk>
2021-04-06 09:47:19 +0100

    Added tick or cross to indicate if test was successfull or not

--- problems/Daily_Coding_Problem/Arrays/Get_Product_Of_All_Other_Elements/Solutions/solution_template.py
+++ problems/Daily_Coding_Problem/Arrays/Get_Product_Of_All_Other_Elements/Solutions/solution_template.py
@@ -1,6 +1,8 @@
-def assert_equals(expected: object, received: object) -> bool:
-    print("Expected: {}, Received: {}".format(expected, received))
-    return expected == received
+def assert_equals(expected: object, received: object):
+    success_character = "\u2717"
+    if expected == received:
+        success_character = "\u2713"
+    print("Expected: {}, Received: {} {}".format(expected, received, success_character))
 
 
 def product_of_all_other_elements(arr: list) -> list:
@@ -10,3 +12,4 @@ def product_of_all_other_elements(arr: list) -> list:
 if __name__ == "__main__":
     assert_equals([120, 60, 40, 30, 24], product_of_all_other_elements([1, 2, 3, 4, 5]))
     assert_equals([2, 3, 6], product_of_all_other_elements([3, 2, 1]))
+
