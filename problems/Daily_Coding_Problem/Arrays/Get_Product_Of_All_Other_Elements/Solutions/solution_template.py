2468e4cdcee6f24ed88179c842f61751f388f9a1
jonathan.westcott <jonathan.westcott123@hotmail.co.uk>
2021-04-06 09:34:16 +0100

    Fixed broken test

--- problems/Daily_Coding_Problem/Arrays/Get_Product_Of_All_Other_Elements/Solutions/solution_template.py
+++ problems/Daily_Coding_Problem/Arrays/Get_Product_Of_All_Other_Elements/Solutions/solution_template.py
@@ -9,4 +9,4 @@ def product_of_all_other_elements(arr: list) -> list:
 
 if __name__ == "__main__":
     assert_equals([120, 60, 40, 30, 24], product_of_all_other_elements([1, 2, 3, 4, 5]))
-    assert_equals([3, 2, 1], product_of_all_other_elements([2, 3, 6]))
+    assert_equals([2, 3, 6], product_of_all_other_elements([3, 2, 1]))
