4e0c9b95d0b8d1239d64763a14a3c8e8b49fab30
jonathan.westcott <jonathan.westcott123@hotmail.co.uk>
2021-04-08 11:02:30 +0100

    Made it clear that the unit tests do not return anything

--- problems/Own_Creation/LinkedLists/Implement_A_Linked_List/Solutions/jwestcott/linked_list_tests.py
+++ problems/Own_Creation/LinkedLists/Implement_A_Linked_List/Solutions/jwestcott/linked_list_tests.py
@@ -8,25 +8,25 @@ class LinkedListTests(unittest.TestCase):
     def setUp(self) -> None:
         self.object_under_test = LinkedList()
 
-    def test_LinkedList_append_correctly_sets_the_start_node_if_no_start_node_currently_exists(self):
+    def test_LinkedList_append_correctly_sets_the_start_node_if_no_start_node_currently_exists(self) -> None:
         test_element = "test"
         self.object_under_test.append(test_element)
         self.assertIsNotNone(self.object_under_test.start_node)
 
-    def test_LinkedList_append_adds_the_correct_data_to_the_start_node_if_no_start_node_currently_exists(self):
+    def test_LinkedList_append_adds_the_correct_data_to_the_start_node_if_no_start_node_currently_exists(self) -> None:
         test_element = "test"
         self.object_under_test.append(test_element)
         self.assertEqual(test_element, self.object_under_test.start_node.stored_object)
 
-    def test_LinkedList_append_adds_the_correct_data_to_the_second_node_if_two_elements_are_added(self):
+    def test_LinkedList_append_adds_the_correct_data_to_the_second_node_if_two_elements_are_added(self) -> None:
         for i in range(2):
             self.object_under_test.append(i)
         self.assertEqual(i, self.object_under_test.start_node.next_node.stored_object)
 
-    def test_LinkedList_count_returns_0_if_not_elements_have_been_added(self):
+    def test_LinkedList_count_returns_0_if_not_elements_have_been_added(self) -> None:
         self.assertEqual(0, self.object_under_test.count())
 
-    def test_LinkedList_count_returns_3_if_3_elements_have_been_added(self):
+    def test_LinkedList_count_returns_3_if_3_elements_have_been_added(self) -> None:
         for i in range(3):
             self.object_under_test.append(i)
         self.assertEqual(3, self.object_under_test.count())
